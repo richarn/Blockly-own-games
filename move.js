f06e283db6a6cb9ccfab303632bb789053630781
richarn <rnezferreira9@gmail.com>
2023-03-07 15:12:39 -0300

    agregado de matrices logicas y graficas

--- move.js
+++ move.js
@@ -1,68 +1,117 @@
-let x = 0;
-var y = 0;
-let cuadradito = document.getElementById('cuadrado');
-/*
-*Al llevar el eje de coordenadas al documento html hacemos una traducción
-*debido a que funcionan distinto y esa diferencia es más evidente cuando trabajamos en el top.
-*
-*En el eje de coordenadas incremetamos para mover hacia arriba, en el top decrementamos
-*para empujar hacias arriba.
-*/
+var x = [
+	[1, 0, 0],
+	[0, 0, 0],
+	[0, 0, 2],
+];
 
-// function movimiento(event){
-// 	if(event.keyCode == '39'){//derecha
-// 		x= x + 100;
-// 		cuadradito.style.left = x + 'px';
-// 	}
-
-// 	if(event.keyCode == '37'){//Izquierda
-// 		x= x - 100;
-// 		cuadradito.style.left = x +'px';	
-// 	}
-	
-// 	if(event.keyCode == '38'){//arriba
-// 		y = y + 100;                               
-// 		cuadradito.style.top = (-y) + 'px';
-// 	}
-
-// 	if(event.keyCode == '40'){//abajo
-// 		y = y - 100;                            
-// 		cuadradito.style.top = (-y) + 'px';
-// 	}
-
-// }
 
 function move_up() {
-	y = y + 96;                               
-	cuadradito.style.top = (-y) + 'px';
+
+	let x2 = x.map(function(arr) {
+		return arr.slice();
+	});
+
+	for (let i = 0; i < x.length; i++) {
+
+		for (let j = 0; j < x[i].length; j++) {
+
+			if (x[i][j] == 1) {
+				if(i<=0){
+					console.log("Fuera de matriz");	
+				}else{
+					if (x2[i-1][j] == 2) {
+						alert("Ganaste!!!").destroy();
+					}
+					x2[i-1][j] = 1;
+					x2[i][j] = 0;
+				}
+			}
+		}
+		
+	}
+	x = x2;
+	updateTable();
 }
 
 function move_down() {
-	y = y - 96;                               
-	cuadradito.style.top = (-y) + 'px';
+	let x2 = x.map(function(arr) {
+		return arr.slice();
+	});
+	for (let i = 0; i < x.length; i++) {
+
+		for (let j = 0; j < x[i].length; j++) {
+
+			if (x[i][j] == 1) {
+				if(i>=(x.length-1)){
+					console.log("Fuera de matriz");	
+				}else{
+					if (x2[i+1][j] == 2) {
+						alert("Ganaste!!!").destroy();
+					}
+					x2[i+1][j] = 1;
+					x2[i][j] = 0;
+				}
+			}
+		}
+		
+	}
+	x = x2
+	updateTable();
 }
 
 function move_left() {
-	x= x - 96;
-	cuadradito.style.left = x +'px';
+	let x2 = x.map(function(arr) {
+		return arr.slice();
+	});
+
+	for (let i = 0; i < x.length; i++) {
+
+		for (let j = 0; j < x[i].length; j++) {
+
+			if (x[i][j] == 1) {
+				if(j<=0){
+					console.log("Fuera de matriz");	
+				}else{
+					if (x2[i][j-1] == 2) {
+						alert("Ganaste!!!").destroy();
+					}
+					x2[i][j-1] = 1;
+					x2[i][j] = 0;
+				}
+			}
+		}
+		
+	}
+	x = x2
+	updateTable();
 }
 
 function move_rigth() {
-	x= x + 96;
-	cuadradito.style.left = x + 'px';
+	let x2 = x.map(function(arr) {
+		return arr.slice();
+	});
+
+	for (let i = 0; i < x.length; i++) {
+
+		for (let j = 0; j < x[i].length; j++) {
+
+			if (x[i][j] == 1) {
+				if(j>=(x[i].length-1)){
+					console.log("Fuera de matriz");	
+				}else{
+					if (x2[i][j+1] == 2) {
+						alert("Ganaste!!!").destroy();
+					}
+					x2[i][j+1] = 1;
+					x2[i][j] = 0;
+				}
+			}
+		}
+		
+	}
+	x = x2
+	updateTable();
 }
-
-// function ballPosition() {
-// 	const element = document.getElementById('cuadrado');
-// 	const ball = element.getBoundingClientRect();
-
-// 	if( ball.top == 341 && ball.left == 1394 ){
-// 		alert("Adentro, lo lograste!").destroy();
-// 	}
-// 	console.log('cuadrado-top:', ball.top, 'cuadrado-left:', ball.left);
-
-// }
-// window.onkeyup = movimiento;
 
 //funcion para bloque mover
 function mover(direccion){
@@ -89,25 +138,54 @@ function mover(direccion){
 	ballPosition();
  }
 
- var table = [];
- var ballStyle = `
-	background-color: red;
-	height: 20px;
-	width: 20px;
-	border-radius: 80px;
-	position: absolute;
- `;
- 
- var tabla = document.getElementById("table");
- for (var i = 0, row; row = tabla.rows[i]; i++) {
+function updateTable() {
+	for (let i = 0; i < x.length; i++) {
 
-	for (var j = 0, col; col = row.cells[j]; j++) {
-		// console.log(`Txt: ${col.innerText} \tFila: ${i} \t Celda: ${j}`);
+		for (let j = 0; j < x[i].length; j++) {
+		if (x[i][j] == 0) {
+			var celda = document.getElementById('x_'+i+'_'+j);
+			celda.style.backgroundColor = "green";
+		}
+		if (x[i][j] == 1) {
+			var celda = document.getElementById('x_'+i+'_'+j);
+			celda.style.backgroundColor = "red";
+		}
+		if (x[i][j] == 2) {
+			var celda = document.getElementById('x_'+i+'_'+j);
+			celda.style.backgroundColor = "blue";
+		}
+
+		}
 	}
 }
-const celda = tabla.rows[0].cells[1];
-celda.style.cssText = ballStyle;
-// console.log(tabla.rows[10].cells[1].style.ballStyle);
- 
+
+function dataTable(x,y){
+
+	var ballStyle = `
+		background-color: red;
+		height: 20px;
+		width: 20px;
+		border-radius: 80px;
+		position: absolute;
+	`;
+	
+	var tabla = document.getElementById("table");
+	for (var i = 0, row; row = tabla.rows[i]; i++) {
+		
+		for (var j = 0, col; col = row.cells[j]; j++) {
+			// console.log(`Txt: ${col.innerText} \tFila: ${i} \t Celda: ${j}`);
+		}
+	}
+	if(x == null){
+		if (y == null) {
+			const defect = tabla.rows[0].cells[0];
+			defect.style.cssText = ballStyle;
+		}
+	}else{
+		const celda = tabla.rows[x].cells[y];
+		celda.style.cssText = ballStyle;
+	}
+} 
+// dataTable();
 
 
